c9ff9be558b3be2c5336b4a202e4c399851e4c83
ebookcode123 <ebookcode123@gmail.com>
2017-04-25 16:29:43 -0700

    add css for screen number; fix del issue

--- popup.js
+++ popup.js
@@ -7,22 +7,31 @@ var operators = ['+', '-', 'x', '÷','%'];
 var decimalAdded = false;
 
 for(var i = 0; i < keys.length; i++) {
+  var result = [];
     keys[i].onclick = function (e) {
         // Get the input and button values
         var input = document.querySelector('.screen');
         var inputVal = input.innerHTML;
         var btnVal = this.innerHTML;
+        result += btnVal;
+        console.log(btnVal);
 
 
-    if(btnVal == 'C') {
-        input.innerHTML = '';
-        decimalAdded = false;
-    }
+        if(btnVal == 'C') {
+            input.innerHTML = '';
+            decimalAdded = false;
+        }
 
-    if(btnVal == '&larr;'){
-        input.innerHTML = inputVal.substr(0,inputVal.length-2);
+        // else if(btnVal == 'Del'){
+        //
+        //   console.log("enter");
+        //
+        // }
 
-    }
+        else{
+          input.innerHTML += btnVal;
+        }
+
 
     }
 }
